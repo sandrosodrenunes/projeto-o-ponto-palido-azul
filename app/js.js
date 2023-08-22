2d9fc7b17b8cb7dd9299d14aee6beb4efa1512ed
Sandro Sodré Nunes <sandrojunior802@gmail.com>
2023-08-22 10:35:34 -0300

    Update js.js

--- app/js.js
+++ app/js.js
@@ -1,7 +1,7 @@
 window.sr = ScrollReveal({reset: true})
 
-sr.reveal('.area-1',{duration: 2000 })
-sr.reveal('.area-2',{duration: 2000 })
-sr.reveal('.area-3',{duration: 2000 })
-sr.reveal('.area-4',{duration: 2000 })
-sr.reveal('.area-5',{duration: 2000 }) 
+sr.reveal('.area-1',{duration: 3000 })
+sr.reveal('.area-2',{duration: 3000 })
+sr.reveal('.area-3',{duration: 3000 })
+sr.reveal('.area-4',{duration: 3000 })
+sr.reveal('.area-5',{duration: 3000 }) 
